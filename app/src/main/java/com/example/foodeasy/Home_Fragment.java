eba41572afcd476388f6d390c188c06ad7dca348
varad <varadsharadkulkarni@gmail.com>
2023-06-25 18:39:11 +0530

    Recycler view implemented but some updates in layout remained

--- app/src/main/java/com/example/foodeasy/Home_Fragment.java
+++ app/src/main/java/com/example/foodeasy/Home_Fragment.java
@@ -3,62 +3,51 @@ package com.example.foodeasy;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Home_Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.foodeasy.databinding.ActivityMarketplaceBinding;
+
+import java.util.ArrayList;
+
+
 public class Home_Fragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Home_Fragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Home_Fragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Home_Fragment newInstance(String param1, String param2) {
-        Home_Fragment fragment = new Home_Fragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    Product_adapter product_adapter;
+    ArrayList<Product>products;
+//    ActivityMarketplaceBinding binding;
+    RecyclerView recyclerView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_, container, false);
+        View view= inflater.inflate(R.layout.fragment_home_, container, false);
+
+        initiateProducts(view);
+
+
+        return view;
+    }
+
+    private void initiateProducts(View view) {
+        products=new ArrayList<>();
+        products.add(new Product("Pizza","Full grilled chicken pizza with legpiece ","https://media.istockphoto.com/id/1192094401/photo/delicious-vegetarian-pizza-on-white.webp?s=1024x1024&w=is&k=20&c=ZQKSxLVVwUgwFEUuDcam6Kc-7IdjpKw4wWA--6HbFZw=",200.00,1));
+        products.add(new Product("Pizza","Full grilled chicken pizza with legpiece ","https://media.istockphoto.com/id/1192094401/photo/delicious-vegetarian-pizza-on-white.webp?s=1024x1024&w=is&k=20&c=ZQKSxLVVwUgwFEUuDcam6Kc-7IdjpKw4wWA--6HbFZw=",200.00,1));
+        products.add(new Product("Pizza","Full grilled chicken pizza with legpiece ","https://media.istockphoto.com/id/1192094401/photo/delicious-vegetarian-pizza-on-white.webp?s=1024x1024&w=is&k=20&c=ZQKSxLVVwUgwFEUuDcam6Kc-7IdjpKw4wWA--6HbFZw=",200.00,1));
+        products.add(new Product("Pizza","Full grilled chicken pizza with legpiece ","https://media.istockphoto.com/id/1192094401/photo/delicious-vegetarian-pizza-on-white.webp?s=1024x1024&w=is&k=20&c=ZQKSxLVVwUgwFEUuDcam6Kc-7IdjpKw4wWA--6HbFZw=",200.00,1));
+        products.add(new Product("Pizza","Full grilled chicken pizza with legpiece ","https://media.istockphoto.com/id/1192094401/photo/delicious-vegetarian-pizza-on-white.webp?s=1024x1024&w=is&k=20&c=ZQKSxLVVwUgwFEUuDcam6Kc-7IdjpKw4wWA--6HbFZw=",200.00,1));
+        products.add(new Product("Pizza","Full grilled chicken pizza with legpiece ","https://media.istockphoto.com/id/1192094401/photo/delicious-vegetarian-pizza-on-white.webp?s=1024x1024&w=is&k=20&c=ZQKSxLVVwUgwFEUuDcam6Kc-7IdjpKw4wWA--6HbFZw=",200.00,1));
+        products.add(new Product("Pizza","Full grilled chicken pizza with legpiece ","https://media.istockphoto.com/id/1192094401/photo/delicious-vegetarian-pizza-on-white.webp?s=1024x1024&w=is&k=20&c=ZQKSxLVVwUgwFEUuDcam6Kc-7IdjpKw4wWA--6HbFZw=",200.00,1));
+        products.add(new Product("Pizza","Full grilled chicken pizza with legpiece ","https://media.istockphoto.com/id/1192094401/photo/delicious-vegetarian-pizza-on-white.webp?s=1024x1024&w=is&k=20&c=ZQKSxLVVwUgwFEUuDcam6Kc-7IdjpKw4wWA--6HbFZw=",200.00,1));
+        product_adapter=new Product_adapter(getContext(),products);
+        recyclerView=view.findViewById(R.id.product_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(product_adapter);
+
     }
 }
---
+++ app/src/main/java/com/example/foodeasy/Product.java
@@ -0,0 +1,55 @@
+package com.example.foodeasy;
+
+public class Product {
+    private String name,description,image;
+    private double price;
+    private int id;
+
+    public Product(String name, String description, String image, double price, int id) {
+        this.name = name;
+        this.description = description;
+        this.image = image;
+        this.price = price;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
